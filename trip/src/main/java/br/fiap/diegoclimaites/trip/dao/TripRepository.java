2ae32326ef75f84e230bdb6404427403bd845be5
Diego Climaites <diegoclimaites@gmail.com>
2020-07-05 13:35:40 -0300

    Adicionado método Find BY Period com query string parameters

--- trip/src/main/java/br/fiap/diegoclimaites/trip/dao/TripRepository.java
+++ trip/src/main/java/br/fiap/diegoclimaites/trip/dao/TripRepository.java
@@ -2,6 +2,13 @@ package br.fiap.diegoclimaites.trip.dao;
 
 import br.fiap.diegoclimaites.trip.model.Trip;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TripRepository {
     private static final DynamoDBMapper mapper = DynamoDBManager.mapper();
@@ -9,5 +16,25 @@ public class TripRepository {
     public Trip save(final Trip trip) {
         mapper.save(trip);
         return trip;
+    }
+
+    public List<Trip> findByPeriod(final String starts, final String ends) {
+
+        final Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":val1", new AttributeValue().withS(starts));
+        eav.put(":val2", new AttributeValue().withS(ends));
+
+        final Map<String, String> expression = new HashMap<>();
+        expression.put("#date", "date");
+
+        //Adicionado o # antes de date, pois, date é uma palavra reservada do Dynamodb
+        final DynamoDBScanExpression queryExpression = new DynamoDBScanExpression()
+                .withFilterExpression("#date between :val1 and :val2")
+                .withExpressionAttributeValues(eav)
+                .withExpressionAttributeNames(expression);
+
+        final List<Trip> trips = mapper.scan(Trip.class, queryExpression);
+
+        return trips;
     }
 }
---
+++ trip/src/main/java/br/fiap/diegoclimaites/trip/handler/GetTripRecordByPeriodFunction.java
@@ -0,0 +1,30 @@
+package br.fiap.diegoclimaites.trip.handler;
+
+import br.fiap.diegoclimaites.trip.dao.TripRepository;
+import br.fiap.diegoclimaites.trip.model.HandlerRequest;
+import br.fiap.diegoclimaites.trip.model.HandlerResponse;
+import br.fiap.diegoclimaites.trip.model.Trip;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.List;
+
+public class GetTripRecordByPeriodFunction implements RequestHandler<HandlerRequest, HandlerResponse> {
+
+    private  final TripRepository repository = new TripRepository();
+
+    @Override
+    public HandlerResponse handleRequest(HandlerRequest handlerRequest, Context context) {
+        final String starts = handlerRequest.getQueryStringParameters().get("starts");
+        final String ends = handlerRequest.getQueryStringParameters().get("ends");
+
+        context.getLogger().log("Procurando viagens entre " + starts + " e " + ends);
+        final List<Trip> trips = this.repository.findByPeriod(starts, ends);
+
+        if(trips == null || trips.isEmpty()) {
+            return HandlerResponse.builder().setStatusCode(404).build();
+        }
+
+        return HandlerResponse.builder().setStatusCode(200).setObjectBody(trips).build();
+    }
+}
