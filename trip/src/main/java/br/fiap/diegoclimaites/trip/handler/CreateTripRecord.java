aa23820cb31661f611200f160df6e0c4445b6a2d
Diego Climaites <diegoclimaites@gmail.com>
2020-07-05 12:08:42 -0300

    Finalizado método de criação de viagem

--- trip/src/main/java/br/fiap/diegoclimaites/trip/handler/CreateTripRecord.java
+++ trip/src/main/java/br/fiap/diegoclimaites/trip/handler/CreateTripRecord.java
@@ -5,25 +5,39 @@ import br.fiap.diegoclimaites.trip.model.HandlerRequest;
 import br.fiap.diegoclimaites.trip.model.HandlerResponse;
 import br.fiap.diegoclimaites.trip.model.Trip;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
-public class CreateTripRecord {
+public class CreateTripRecord implements RequestHandler<HandlerRequest, HandlerResponse> {
     private  final TripRepository repository = new TripRepository();
 
+    @Override
     public HandlerResponse handleRequest(final HandlerRequest request, final Context context) {
         Trip trip = null;
 
         try {
+            context.getLogger().log("Entrou no try \n");
+            context.getLogger().log("Valor do body \n" );
+            context.getLogger().log(request.getBody() + "\n" );
             trip = new ObjectMapper().readValue(request.getBody(), Trip.class);
+            context.getLogger().log("Termino o try \n");
         }
         catch (IOException e) {
-            return HandlerResponse.builder().setStatusCode(500).setRawBody("Erro ao tentar salvar Trip").build();
+            String erro = "Erro ao tentar salvar Trip \n";
+            context.getLogger().log("Stack \n");
+            context.getLogger().log(e.getStackTrace().toString() + "\n");
+            return HandlerResponse.builder().setStatusCode(500).setRawBody(erro + e.getMessage()).build();
         }
 
-        context.getLogger().log("Criou uma Trip nova" + trip.getCity());
+        context.getLogger().log("Verificando entidade \n");
+        context.getLogger().log( trip.getCountry() + "\n");
+        context.getLogger().log( trip.getCity() +"\n");
+        context.getLogger().log(trip.getDate().toString() + "\n");
+        context.getLogger().log(trip.getReason() + "\n");
         final Trip tripRecorded = repository.save(trip);
+        context.getLogger().log("Criou uma Trip nova" + trip.getCity());
         return HandlerResponse.builder().setStatusCode(201).setObjectBody(tripRecorded).build();
     }
 }
--- trip/src/main/java/br/fiap/diegoclimaites/trip/model/Trip.java
+++ trip/src/main/java/br/fiap/diegoclimaites/trip/model/Trip.java
@@ -6,54 +6,58 @@ import java.util.Date;
 
 @DynamoDBTable(tableName = "trip")
 public class Trip {
+
+    @DynamoDBHashKey(attributeName = "country")
+    private String country;
+    @DynamoDBIndexRangeKey(attributeName = "city", localSecondaryIndexName = "cityIndex")
+    private String city;
+
+    @DynamoDBRangeKey(attributeName = "date")
+    private String date;
+
+    @DynamoDBAttribute(attributeName = "reason")
+    private String reason;
+
     public String getCountry() {
         return country;
     }
 
     public void setCountry(String country) {
-        country = country;
+        this.country = country;
     }
 
     public String getCity() {
-        return city;
+        return this.city;
     }
 
     public void setCity(String city) {
-        city = city;
+        this.city = city;
     }
 
 
-    public Date getDate() {
-        return date;
+    public String getDate() {
+        return this.date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
 
     public String getReason() {
-        return reason;
+        return this.reason;
     }
 
     public void setReason(String reason) {
-        reason = reason;
+        this.reason = reason;
     }
 
-    public Trip(String country, String city, Date date, String reason) {
-        country = country;
-        city = city;
+    public Trip(String country, String city, String date, String reason) {
+        super();
+        this.country = country;
+        this.city = city;
         this.date = date;
-        reason = reason;
+        this.reason = reason;
     }
 
-    public Trip() {}
-
-    @DynamoDBHashKey(attributeName = "country")
-    private String country;
-    @DynamoDBAttribute(attributeName = "city")
-    private String city;
-    @DynamoDBIndexRangeKey(attributeName = "date", localSecondaryIndexName = "dateIndex")
-    private Date date;
-    @DynamoDBAttribute(attributeName = "reason")
-    private String reason;
+    public Trip() { super(); }
 }
